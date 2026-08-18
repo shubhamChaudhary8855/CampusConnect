110330da33f07951c0a9715c26187877da2ea8df
Shubham Chaudhary <chaudharyshubham84954@gmail.com>
2026-08-18 08:17:57 +0530

    feat: add CampusConnect application entry point

---
+++ src/campusconnect/Main.java
@@ -0,0 +1,10 @@
+package campusconnect;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("================================");
+        System.out.println("        CAMPUS CONNECT");
+        System.out.println("================================");
+        System.out.println("Version 1 - Project foundation");
+    }
+}
