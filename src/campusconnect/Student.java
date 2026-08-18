6bf88423be97345921b4a2e86cd8c97f26f56d66
Shubham Chaudhary <chaudharyshubham84954@gmail.com>
2026-08-18 08:18:04 +0530

    feat: add Student domain model

---
+++ src/campusconnect/Student.java
@@ -0,0 +1,34 @@
+package campusconnect;
+
+public class Student {
+    private final int id;
+    private final String name;
+    private final String email;
+
+    public Student(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
